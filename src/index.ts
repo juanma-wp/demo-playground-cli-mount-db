0e415751b1591dde05e4fcb53d66ec2c143142f8
JuanMa <juanma.garrido@gmail.com>
2025-07-04 13:36:02 +0100

    Enhance WordPress Playground functionality by adding JWT authentication and user info retrieval
    - Updated index.ts to load blueprint.json from the correct path and implement JWT authentication for user login.
    - Added requests to retrieve user information, including last login, registration date, role, and capabilities, enhancing the API's user data output.
    - Created blueprint.json to define installation steps for the JWT authentication plugin and configuration constants.
    - Introduced extended-user-info-rest.php plugin to extend the REST API with additional user data fields.

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,46 @@ import { resolve } from "path";
 (async () => {
   // Load blueprint.json
   const blueprint = JSON.parse(
-    readFileSync(resolve("./blueprint.json"), "utf8")
+    readFileSync(resolve("./wordpress/blueprint.json"), "utf8")
   );
   const requestHandler = await createPlaygroundRequestHandler(blueprint);
 
-  // Make a sample request to /wp-json/wp/v2/posts
-  const req = {
-    method: "GET",
-    url: "/wp-json/wp/v2/posts",
-    headers: {},
-  } as PHPRequest;
+  const reqGetToken = {
+    method: "POST",
+    url: `/wp-json/jwt-auth/v1/token`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: {
+      username: "admin",
+      password: "password",
+    },
+  };
 
-  const res = await requestHandler.request(req);
-  // const res = await requestFollowRedirects(requestHandler, req);
-  console.log("Response:", res.text, res.httpStatusCode, res.headers);
+  const resGetToken = await requestHandler.request(reqGetToken as PHPRequest);
+  const { token, user_email, user_nicename, user_display_name } = JSON.parse(
+    resGetToken.text
+  );
+  
+  console.log(
+    "Response:",
+    resGetToken.httpStatusCode,
+    resGetToken.headers
+  );
+  console.log({ token, user_email, user_nicename, user_display_name });
+  
+  const reqGetUserInfo = {
+    method: "GET",
+    url: `/wp-json/wp/v2/users/me`,
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`,
+    },
+  };
+  const resGetUserInfo = await requestHandler.request(
+    reqGetUserInfo as PHPRequest
+  );
+  const { id, name, url, description, link, slug, avatar_urls, meta, _links } = JSON.parse(resGetUserInfo.text);
+  console.log("Response:", resGetUserInfo.httpStatusCode, resGetUserInfo.headers);
+  console.log({ id, name, url, description, link, slug, avatar_urls, meta, _links });
 })(); 
